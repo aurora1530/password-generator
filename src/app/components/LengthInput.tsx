6a6bfc769ee23ecc45b80727db5684fe279c506f
aurora1530 <k.0523.y.1530@gmail.com>
2024-01-28 22:17:35 +0900

    +と-ボタンを追加。

--- src/app/components/LengthInput.tsx
+++ src/app/components/LengthInput.tsx
@@ -1,24 +1,36 @@
-import { FormControl, FormLabel, Input } from '@mui/material';
+import { Box, Button, FormControl, FormLabel, Input } from '@mui/material';
 import { useRecoilState } from 'recoil';
 import { lengthAtom } from './atom';
-
+import { Add, Remove } from '@mui/icons-material';
 export default function LengthInput() {
   const [length, setLength] = useRecoilState(lengthAtom);
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setLength(Number(event.target.value));
   };
+  const handleAdd = () => setLength((pre) => pre + 1);
+  const handleRemove = () =>
+    setLength((pre) => {
+      if (pre > 1) return pre - 1;
+      return pre;
+    });
   return (
-    <>
-      <FormControl required>
-        <FormLabel>パスワードの長さ</FormLabel>
+    <FormControl required>
+      <FormLabel>パスワードの長さ</FormLabel>
+      <Box display="flex">
         <Input
           type="number"
-          defaultValue={length}
+          value={length}
           inputProps={{ min: '1', step: '1' }}
           style={{ width: '4rem' }}
           onChange={handleChange}
         />
-      </FormControl>
-    </>
+        <Button variant="outlined" size="small" onClick={handleAdd}>
+          <Add fontSize="small" />
+        </Button>
+        <Button variant="outlined" size="small" onClick={handleRemove}>
+          <Remove fontSize="small" />
+        </Button>
+      </Box>
+    </FormControl>
   );
 }
