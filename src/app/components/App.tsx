c0aefb88561ca3cac7603547d286a725454e0c8e
aurora1530 <k.0523.y.1530@gmail.com>
2024-01-28 17:36:06 +0900

    appにもthemeを適用

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,32 +1,36 @@
 'use client';
 
-import { Box } from '@mui/material';
+import { Box, CssBaseline, ThemeProvider } from '@mui/material';
 import IncludeCharCheckBox from './IncludeCharCheckBox';
 import GenButton from './GenButton';
 import { RecoilRoot } from 'recoil';
 import LengthInput from './LengthInput';
 import ExcludeCharCheckBox from './ExcludeCharCheckBox';
 import OptionsCard from './OptionCard';
+import theme from '../theme';
 
 export default function App() {
   return (
     <>
-      <RecoilRoot>
-        <Box>
-          <OptionsCard>
-            <GenButton />
-          </OptionsCard>
-          <OptionsCard>
-            <IncludeCharCheckBox />
-          </OptionsCard>
-          <OptionsCard>
-            <LengthInput />
-          </OptionsCard>
-          <OptionsCard>
-            <ExcludeCharCheckBox />
-          </OptionsCard>
-        </Box>
-      </RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RecoilRoot>
+          <Box>
+            <OptionsCard>
+              <GenButton />
+            </OptionsCard>
+            <OptionsCard>
+              <IncludeCharCheckBox />
+            </OptionsCard>
+            <OptionsCard>
+              <LengthInput />
+            </OptionsCard>
+            <OptionsCard>
+              <ExcludeCharCheckBox />
+            </OptionsCard>
+          </Box>
+        </RecoilRoot>
+      </ThemeProvider>
     </>
   );
 }
